809037be981d3d0c5d2b527498ca8fde68e53fd6
tanliwei <3063150535@qq.com>
2025-10-28 16:26:42 +0800

    MyBatisPlus

---
+++ src/main/java/com/itheima/mp/mapper/UserMapper.java
@@ -0,0 +1,21 @@
+package com.itheima.mp.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.mapper.Mapper;
+import com.itheima.mp.domain.po.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface UserMapper extends BaseMapper<User> {
+
+//    void saveUser(User user);
+//
+//    void deleteUser(Long id);
+//
+//    void updateUser(User user);
+//
+//    User queryUserById(@Param("id") Long id);
+//
+//    List<User> queryUserByIds(@Param("ids") List<Long> ids);
+}
